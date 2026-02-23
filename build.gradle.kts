b2928d53eb0c59adf5153b617fec67bc66546a4d
Luke Bemish <lukebemish@lukebemish.dev>
2026-02-22 20:58:46 -0500

    Set up reproducer

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+val someAttribute = Attribute.of("someAttribute", String.class)
+
+val variantName by configurations.consumable {
+    attributes {
+        attribute(someAttribute, "gizmo")
+    }
+    outgoing.variants {
+        register("secondaryArtifactSet") {
+            attributes {
+                attribute(someAttribute, "gadget")
+            }
+        }
+    }
+}
+
+val stuff by configurations.dependencyScope
+val stuffResolved by configurations.resolvable {
+    extendsFrom(stuff)
+    attributes {
+        attribute(someAttribute, "gizmo")
+    }
+}
+
+dependencies {
+    add("stuff", project(":"))
+}
+
+tasks.register("resolveStuff") {
+    dependsOn(stuffResolved)
+}
